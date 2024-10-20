2597975867be0f28b1ded46b9351d9386ce86f5e
Maria Luiza Rodrigues <marialuiza.r.eng@gmail.com>
2024-10-20 19:41:35 -0300

    feat: adding form structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,36 @@
 export function App() {
-  return <h1 className="text-3xl underline">Hello world!</h1>;
+  return (
+    <form action="">
+      <label htmlFor="name">Nome completo</label>
+      <input type="text" />
+      <label htmlFor="email">E-mail</label>
+      <input type="email" />
+      <label htmlFor="phone">Telefone</label>
+      <input type="tel" />
+      <label htmlFor="role">Cargo pretendido</label>
+      <select name="role" id="role">
+        <option value="">Selecione</option>
+        <option value="front-end-developer">Desenvolvedor Front-end</option>
+        <option value="back-end-developer">Desenvolvedor Back-end</option>
+        <option value="full-stack-developer">Desenvolvedor Full-stack</option>
+        <option value="mobile-developer">Desenvolvedor Mobile</option>
+        <option value="software-developer">Desenvolvedor de Software</option>
+        <option value="software-engineer">Engenheiro de Software</option>
+        <option value="software-architect">Arquiteto de Software</option>
+        <option value="ux-ui-designer">UX/UI Designer</option>
+        <option value="system-analyst">Analista de Sistemas</option>
+        <option value="programmer-analyst">Analista Programador</option>
+        <option value="devops-engineer">DevOps Engineer</option>
+        <option value="data-engineer">Engenheiro de Dados</option>
+        <option value="qa-engineer">QA Engineer</option>
+        <option value="scrum-master">Scrum Master</option>
+        <option value="product-owner">Product Owner</option>
+      </select>
+      <label htmlFor="linkedin">Linkedin</label>
+      <input type="url" />
+      <label htmlFor="github">Github</label>
+      <input type="url" />
+      <button type="submit">Enviar</button>
+    </form>
+  );
 }
